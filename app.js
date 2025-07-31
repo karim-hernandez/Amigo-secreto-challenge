4843efa8b9e3021d4691cd892a6972067b07d5ac
karim-hernandez <kg.hernandez@ugto.mx>
2025-07-31 15:42:48 -0600

    agrega lista agrega amigos a lista, checa entrada en blanco y limpia campo de amigo

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// variables
+let amigos = [];
+
+
+//funciones 
+
+function agregarAmigo () {
+       let amigo = document.getElementById('amigo').value;
+       console.log(amigo)
+       if (amigo == '') {
+        alert("Por favor inserte un nombre");
+        } 
+        else {
+            if (amigos.includes(amigo)) {
+                alert("amigo ya incluido");
+            } 
+            else {
+                amigos.push(amigo);
+            }
+            console.log(amigos)
+            limpiarEntradaAmigo();
+        }
+    }
+function sortearAmigo() {
+}
+function limpiarEntradaAmigo () {
+    document.querySelector('#amigo').value = '';
+
+}
