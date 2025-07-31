0d785261e6469a1c5e0dd0b2e19d5cc283003f8f
karim-hernandez <kg.hernandez@ugto.mx>
2025-07-31 16:26:51 -0600

    agrega mostrar lista amigos

--- app.js
+++ app.js
@@ -5,25 +5,42 @@ let amigos = [];
 //funciones 
 
 function agregarAmigo () {
-       let amigo = document.getElementById('amigo').value;
-       console.log(amigo)
-       if (amigo == '') {
-        alert("Por favor inserte un nombre");
+    let amigo = document.getElementById('amigo').value;
+    console.log(amigo)
+    if (amigo == '') {
+    alert("Por favor inserte un nombre");
+    } 
+    else {
+        if (amigos.includes(amigo)) {
+            alert("amigo ya incluido");
         } 
         else {
-            if (amigos.includes(amigo)) {
-                alert("amigo ya incluido");
-            } 
-            else {
-                amigos.push(amigo);
-            }
-            console.log(amigos)
-            limpiarEntradaAmigo();
+            amigos.push(amigo);
         }
+        console.log(amigos)
+        limpiarEntradaAmigo();
+        limpiarListaAmigos();
+        mostrarAmigos(amigos);
     }
+}
+
+function mostrarAmigos(amigos) {
+    let listaAmigos = document.getElementById('listaAmigos');
+    for ( let indice = 0; indice < amigos.length; indice++ ) {
+        let elementoLista = document.createElement('li');
+        elementoLista.innerText = amigos[indice];
+        listaAmigos.appendChild(elementoLista);
+        listaAmigos.re
+    }
+}
 function sortearAmigo() {
 }
+
 function limpiarEntradaAmigo () {
     document.querySelector('#amigo').value = '';
+}
 
+
+function limpiarListaAmigos () {
+    document.querySelector('#listaAmigos').innerHTML = '';
 }
